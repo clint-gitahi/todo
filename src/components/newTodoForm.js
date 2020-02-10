68c871790efe6bee3660d9cbb7580f7eeca811db
clint-gitahi <clingitahi@gmail.com>
2020-02-10 09:04:36 +0300

    handling form value and submit

--- src/components/newTodoForm.js
+++ src/components/newTodoForm.js
@@ -5,14 +5,33 @@ class NewTodoForm extends Component {
     super(props);
 
     this.state = { task: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.prevenDefault();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor="task">New Todo</label>
 
-        <input type="text" placeholder="new todo" />
+        <input
+          type="text"
+          placeholder="new todo"
+          name="task"
+          onChange={this.handleChange}
+          value={this.state.task}
+        />
         <button>Add Todo</button>
       </form>
     );
