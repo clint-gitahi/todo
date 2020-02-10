059345ac42e24b1965b35316054828020a4c1b6a
clint-gitahi <clingitahi@gmail.com>
2020-02-10 08:30:31 +0300

    initial component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 
-import App from './components/TodoList';
+import App from './components/App';
 
 const rootElem = document.getElementById('root');
 
